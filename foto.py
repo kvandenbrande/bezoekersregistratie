f8d3645c2e3bb0b4a8b03f7d06872042133f3a59
Kevin Van den Brande <kvandenbrande@gmail.com>
2016-08-25 12:17:48 +0200

    modified command to create photo

--- foto.py
+++ foto.py
@@ -1,10 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # _*_ coding: utf-8 _*_
 #say cheese :-) 
 
 import picamera
 from time import sleep
+import os
 
-def maakfoto():
-    camera = picamera.PiCamera()
-    camera.capture('onthaal.jpg')
+def maakfoto(fotonaam):
+    #camera = picamera.PiCamera()
+    #camera.capture(fotonaam)
+    command='raspistill -o '+fotonaam+' -t 5 -w 800 -h 480 -n'
+    os.system(command)
