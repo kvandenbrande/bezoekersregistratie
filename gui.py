2bf563e3dd71ee1f513dc349613056582a479ff8
Kevin Van den Brande <kvandenbrande@gmail.com>
2016-08-25 12:06:15 +0200

    view change

--- gui.py
+++ gui.py
@@ -1,30 +1,55 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 
+class GUI:
 
-def raise_frame(frame):
-    frame.tkraise()
+    def __init__(self, master):
+        
+        master.title('Bezoekersregistratie')
+        master.resizable(False, False)
+        master.minsize(width=800, height=480)
+        master.configure(background = '#FA9D00')
+        
+        self.style = ttk.Style()
+        self.style.configure('TFrame', background = '#FA9D00')
+        self.style.configure('TButton', background = '#ADD8E6')
+        self.style.configure('TLabel', background = '#FA9D00', font = ('Arial', 11))
+        self.style.configure('Header.TLabel', font = ('Arial', 18, 'bold'))      
 
-root = Tk()
+        self.frame_header = ttk.Frame(master)
+        self.frame_header.pack()
+        
+        self.logo = PhotoImage(file = 'kvkaw.gif')
+        ttk.Label(self.frame_header, image = self.logo).grid(row = 0, column = 0, rowspan = 5)
+        ttk.Label(self.frame_header, wraplength = 600, text = 'Welkom bij Voka-Kamer van Koophandel Antwerpen-Waasland', style = 'Header.TLabel').grid(row = 1, column = 1)
+        
+        
+        self.frame_content = ttk.Frame(master)
+        self.frame_content.pack()
 
-f1 = Frame(root)#start
-f2 = Frame(root)#visitor
-f3 = Frame(root)#event
-f4 = Frame(root)
+        
+        ttk.Button(self.frame_content, text = 'BEZOEK / MEETING',
+                   command = self.submit).grid(row = 4, column = 0, padx = 5, pady = 5, sticky = 'e')
+        ttk.Button(self.frame_content, text = 'OPLEIDING / EVENT',
+                   command = self.clear).grid(row = 4, column = 1, padx = 5, pady = 5, sticky = 'w')
 
-for frame in (f1, f2, f3, f4):
-    frame.grid(row=0, column=0, sticky='news')
-
-Button(f1, text='Go to frame 2', command=lambda:raise_frame(f2)).pack()
-Label(f1, text='FRAME 1').pack()
-
-Label(f2, text='FRAME 2').pack()
-Button(f2, text='Go to frame 3', command=lambda:raise_frame(f3)).pack()
-
-Label(f3, text='FRAME 3').pack(side='left')
-Button(f3, text='Go to frame 4', command=lambda:raise_frame(f4)).pack(side='left')
-
-Label(f4, text='FRAME 4').pack()
-Button(f4, text='Goto to frame 1', command=lambda:raise_frame(f1)).pack()
-
-raise_frame(f1)
-root.mainloop()
+    def submit(self):
+        print('Name: {}'.format(self.entry_name.get()))
+        print('Email: {}'.format(self.entry_email.get()))
+        print('Comments: {}'.format(self.text_comments.get(1.0, 'end')))
+        self.clear()
+        #messagebox.showinfo(title = 'Safety Feedback', message = 'Thank you for making you voice heard!')
+    
+    def clear(self):
+        self.entry_name.delete(0, 'end')
+        self.entry_email.delete(0, 'end')
+        self.text_comments.delete(1.0, 'end')
+         
+def main():            
+    
+    root = Tk()
+    gui = GUI(root)
+    root.mainloop()
+    
+if __name__ == "__main__": main()
