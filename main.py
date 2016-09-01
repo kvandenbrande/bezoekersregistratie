28723784c32feb224f2fa7cd5e14c80a4856fd9d
Kevin Van den Brande <kvandenbrande@gmail.com>
2016-09-01 22:14:47 +0200

    extracted mailing part

--- main.py
+++ main.py
@@ -1,102 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from versturen import sending
 from foto import maakfoto
 from ftp import ftpfoto
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
 import time,os,socket
 import configparser
 import dbacties
-
-#-------------------------------------------------------------------------------------------------------------
-def mailtodbbeheerder(dbbeheerder,onderneming,naam,voornaam,email):
-    
-    # Create message container - the correct MIME type is multipart/alternative.
-    subject = 'Een bezoeker wenst onze nieuwsbrief te ontvangen'  
-    sender = 'Onthaal'
-      
-    msgRoot = MIMEMultipart('related')  
-    msgRoot['Subject'] = subject
-    msgRoot['From'] = sender
-
-    html = \
-            """\
-            <html>
-              <head></head>
-              <body>
-                <p>Beste,<br><br>
-                   Bezoeker met onderstaande gegevens wenst onze nieuwsbrief te ontvangen.<br><br>
-                   Onderneming: {onderneming} <br> Naam: {naam} <br>Voornaam: {voornaam} <br>Email: {email} <br><br><img src="cid:image1">
-                </p>
-               </body>
-            </html>
-            """.format(**locals())  
-
-    msgText = MIMEText(html,'html')
-
-      
-    msgRoot.attach(msgText)  
-      
-    msg = msgRoot.as_string().encode('utf-8')
-
-    # Send the message via SMTP server.
-    sending(dbbeheerder,msg)
-
-#-------------------------------------------------------------------------------------------------------------
-def mailcontact(
-    # Create the body of the message
-    receiver,
-    contact,
-    onderneming,
-    naam,
-    voornaam,
-    email,
-    fotonaam
-    ):
-    
-    # Create message container - the correct MIME type is multipart/alternative.
-    subject = 'Uw bezoeker is toegekomen'  
-    sender = 'Onthaal'
-    attachement  = fotonaam
-      
-    msgRoot = MIMEMultipart('related')  
-    msgRoot['Subject'] = subject
-    msgRoot['From'] = sender
-
-    html = \
-            """\
-            <html>
-              <head></head>
-              <body>
-                <p>Beste {contact},<br><br>
-                   Uw bezoeker met onderstaande gegevens is toegekomen.<br><br>
-                   Onderneming: {onderneming} <br> Naam: {naam} <br>Voornaam: {voornaam} <br>Email: {email} <br><br><img src="cid:image1">
-                </p>
-               </body>
-            </html>
-            """.format(**locals())  
-
-    msgText = MIMEText(html,'html')
-
-      
-    msgRoot.attach(msgText)  
-      
-    fp = open(attachement, 'rb')  
-    msgImage = MIMEImage(fp.read())  
-    fp.close()  
-      
-    msgImage.add_header('Content-ID', '<image1>')  
-    msgRoot.attach(msgImage)  
-
-
-    msg = msgRoot.as_string().encode('utf-8')
-
-    # Send the message via SMTP server.
-    sending(receiver,msg)
-
+import versturen
     
 #-------------------------------------------------------------------------------------------------------------
 def contacttodatabase(
